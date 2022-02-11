66df85cc9aa54443ba433a97b29988a2d2ca1480
Fabio Greenwood <96693924+FabioGreenwood@users.noreply.github.com>
2022-02-11 11:18:36 +0000

    progress on populate_input_actor_scores_for_film, #find the top 10 actors per film, and convert into single values of their expected film score for a given year

--- programming_analysis_project_analysis_file.py
+++ programming_analysis_project_analysis_file.py
@@ -114,9 +114,54 @@ p=sns.diverging_palette(250, 30, l=65, center="dark", as_cmap=True)
 
 #%% Draft - Parallel Axis Chart
 
-fig = px.parallel_coordinates(md_PrimaryActorsList_2)
+fig2 = px.parallel_coordinates(md_PrimaryActorsList_2)
 fig.show()
 
 
-#%%
+#%% General Methods
 
+def populate_input_actor_scores_for_film(film_tconst, desired_number_of_primary_actors, desired_number_of_secondary_actors):
+    #find every relavent primary actor, as select the ones with the highest film count
+    """ find a way to filter according to index"""
+    
+    
+    primary_actors_in_film = md_actor_to_film[md_actor_to_film.index.get_level_values('tconst') == film_tconst]
+    primary_actors_in_film['count'] = None
+    
+    for index in primary_actors_in_film.index:
+        row_in_df = md_PrimaryActorsList.loc[md_PrimaryActorsList['Name'] == primary_actors_in_film['actor name'][index]].index[0]
+        #primary_actors_in_film['count'][index] = len(md_PrimaryActorsList['tconst'][row_in_df])
+        primary_actors_in_film.loc[index, 'count'] = len(md_PrimaryActorsList['tconst'][row_in_df])
+    
+    primary_actors_to_be_populated_qty = min(desired_number_of_primary_actors, len(primary_actors_in_film))
+    actors_to_populate_tconsts = np.array([])
+    
+    for i in range(0, primary_actors_to_be_populated):
+        max_count = primary_actors_in_film['count'].max()
+        target_index = primary_actors_in_film.loc[primary_actors_in_film['count'] == max_count].index
+        actors_to_populate_tconsts = np.append(actors_to_populate_tconsts, target_index[0][0])
+        primary_actors_in_film = primary_actors_in_film.drop(target_index)
+        
+        
+def return_tconsts_in_film_with_highest_film_counts(film_tconst, actors_qty, actor_DB, previous_tconst):
+    # called twice by populate_input_actor_scores_for_film
+    if len(previous_tconst) = 0:
+        tconsts = np.array([])
+    else:
+        tconsts = previous_tconst
+    return tconsts    
+    
+    
+    
+    
+    
+    
+    
+
+#filtered_primary_actor_list = print(md_actor_to_film[np.in1d(md_actor_to_film.index.get_level_values(1), [film_tconst])])
+film_tconst = 'tt1067106'
+populate_input_actor_scores_for_film(film_tconst, 0)
+
+#md_actor_to_film[(nm0001715, tt0118528)]
+
+#print(md_actor_to_film[np.in1d(md_actor_to_film.index.get_level_values(0), [film_tconst])])
